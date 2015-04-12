e7fbee1216fb4cb74fc546f1a47420d95c220c61
Hassan Arouss <hassan.arouss@silkendream.fr>
2015-04-12 16:44:12 +0200

    Detailed alert & more infos

--- brutaldoom.sh
+++ brutaldoom.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+########################################
+#        BRUTAL DOOM LAUNCHER
+#
+# Author...: Hassan ElAzzouzi
+# Version..: 1.0
+# Date.....: 12/04/2015
+#######################################
+
 GZDOOM_PATH=/usr/bin/gzdoom
 ZANDRONUM_PATH=/usr/bin/zandronum
 WAD_FILE=doom1.wad
@@ -10,6 +19,6 @@ then
     $ZANDRONUM_PATH $WAD_FILE $MUSIC_FILE $BRUTAL_FILE 
     exit 0
 else
-    echo "Zandronum is not installed"
-    zenity --info --text="Zandronum is not installed"
+    echo "Zandronum is not installed, also, check if brutal19.pk3 and DoomMetalVol4.wad are not missing"
+    zenity --info --text="Zandronum is not installed or/and Wad/pk3 missing"
 fi
