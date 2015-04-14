2c4de5b0610f4032b80e66675e25cf8bd39899d6
CapLen <hassan.el_azzouzi@edu.univ-fcomte.fr>
2015-04-14 14:13:50 +0200

    Update launcher

--- brutaldoom.sh
+++ brutaldoom.sh
@@ -21,4 +21,16 @@ then
 else
     echo "Zandronum is not installed, also, check if brutal19.pk3 and DoomMetalVol4.wad are not missing"
     zenity --info --text="Zandronum is not installed or/and Wad/pk3 missing"
+    read -r -p "Do you want to install Zandronum ? [y/N]" input
+    case $input in
+    [yY])
+        sudo apt-add-repository 'deb http://debian.drdteam.org/ stable multiverse'
+        wget -O - http://debian.drdteam.org/drdteam.gpg | sudo apt-key add -
+        sudo apt-get update
+        sudo apt-get install zandronum
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
 fi
